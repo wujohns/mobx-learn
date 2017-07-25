9badf9abf7ba67f459a6c92667a35fba91e3da01
wujohns <wujohns@163.com>
2017-07-25 21:16:01 +0800

    完成base1

--- scripts/base1.js
+++ scripts/base1.js
@@ -6,11 +6,25 @@
  */
 import { observable, computed, autorun } from 'mobx';
 
+// 会触发 autorun 的场景
 const numbers = observable([1, 2, 3]);
-const sum = computed(() => numbers.reduce((a, b) => a + b, 0));
+const sum = computed(() => {
+    console.log('compute the sum of numbers');
+    return numbers.reduce((a, b) => a + b, 0);
+});
 
 const disposer1 = autorun(() => console.log(`sum: ${ sum.get() }`));
 const disposer2 = autorun(() => console.log(`length: ${ numbers.length }`));
+
+// 不会触发 autorun 的场景（只会在第一次时触发）
+const staticNums = observable([1, 2, 3]);
+const staticSum = computed(() => {
+    console.log('static sum');
+    return staticNums.reduce((a, b) => a + b, 0);
+});
+
+const disposer3 = autorun(() => console.log(`staticSum: ${ staticSum.get() }`));
+const disposer4 = autorun(() => console.log(`staticlength: ${ staticNums.length }`));
 
 numbers.push(4);
 numbers.push(5);
